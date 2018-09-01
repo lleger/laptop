ce74264ab6cbcdf1cada64f70a91c744d18b064c
Logan Leger <logan@loganleger.com>
2018-09-01 13:40:32 -0500

    Add script to copy all dotfiles automatically

---
+++ dotfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -ex
+
+cp dotfiles/default-gems ~/.default-gems
+cp dotfiles/asdfrc ~/.asdfrc
+cp -R dotfiles/bundle ~/.bundle
+cp -R dotfiles/oh-my-zsh-custom/plugins/loganleger ~/.oh-my-zsh/custom/plugins/
+cp dotfiles/agignore ~/.agignore
+cp dotfiles/ctags ~/.ctags
+cp dotfiles/gemrc ~/.gemrc
+cp dotfiles/gitconfig ~/.gitconfig
+cp dotfiles/gitignore ~/.gitignore
+cp dotfiles/hushlogin ~/.hushlogin
+cp dotfiles/psqlrc ~/.psqlrc
+cp dotfiles/tm_properties ~/.tm_properties
+cp dotfiles/tmux.conf ~/.tmux.conf
+cp dotfiles/vimrc ~/.vimrc
+cp dotfiles/vimrc.bundles ~/.vimrc.bundles
+cp dotfiles/vimrc.local ~/.vimrc.local
+cp dotfiles/zshrc ~/.zshrc
